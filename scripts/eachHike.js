b4f38d6c43963a4e645bbbe710247c2c1844a493
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-02-28 10:27:10 -0800

    Finished demo.

---
+++ scripts/eachHike.js
@@ -0,0 +1,18 @@
+function displayHikeInformation() {
+    // Retreive the document id from the URL
+    let params = new URL(window.location.href) // get the url from the search bar.
+    let ID = params.searchParams.get("docID");
+    console.log(ID);
+
+    db.collection("hikes").doc(ID).get().then(thisHikes => {
+        hikeInfo = thisHikes.data();
+        hikeCode = hikeInfo.code;
+        hikeName = hikeInfo.name;
+
+        document.getElementById("hikeName").innterHTML = hikeName;
+        let imgEvent = document.querySelector(".hike-img");
+        imgEvent.src = "../images/" + hikeCode + ".jpg";
+    });
+}
+
+displayHikeInformation();
